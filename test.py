bfcb9c2b6fe1be2f62d090f90db7bdec8c1f37b3
Ahmad <pouramini@gmail.com>
2021-03-19 13:29:33 +0330

    ... Adding test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -8,10 +9,15 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.5.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
 import logging
 import sacrebleu
+from pathlib import Path
 import pandas as pd
 import sys
 from simpletransformers.t5 import T5Model, T5Args
@@ -22,38 +28,77 @@ transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
 
 model_args = T5Args()
-model_args.max_length = 512
-model_args.length_penalty = 1
-model_args.num_beams = 10
+model_args.max_length = 50
+model_args.length_penalty = 2.0
+model_args.repetition_penalty = 2.0
+model_args.num_beams = 5
+model_args.early_stopping = True
+#model_args.do_sample = True
+#model_args.top_p = 0.3
+model_args.num_return_sequences = 5
+
 
 #model = T5Model("mt5", "persiannlp/mt5-base-parsinlu-opus-translation_fa_en", args=model_args)
-model_name = "t5-small"
-result_name = "xWant"
-model = T5Model("t5", model_name + "/best_model", args=model_args)
-
-eval_df = pd.read_csv("data/eval.tsv", sep="\t").astype(str)
-
-truth_values = eval_df.loc[eval_df["prefix"] == "xWant"]["target_text"].tolist()
-input_values = eval_df.loc[eval_df["prefix"] == "xWant"]["input_text"].tolist()
-
-pred_values = model.predict(input_values)
-
-# +
-bleu_score = sacrebleu.corpus_bleu(pred_values, truth_values)
-print(bleu_score.score)
-
-with open(f'{model_name}/results/{result_name}.txt', 'w') as f:
-        print("BLEU:" + bleu_score.score, file=f)
-        for truth, pred in zip(truth_values, pred_values):
-            print(truth,"--", pred)
-            print(truth,"--", pred, file =f)
-# -
-
-print("--------------------------")
-print("BLEU: ", bleu_score.score)
-
+model_name = "outputs_t5_small_full_2020/"
+task = "xWant"
+model = T5Model("t5", model_name, args=model_args)
 
 print("predicting")
-print(model.predict(["PersonX go to the bar","PersonX hunts"]))
+#print(model.predict(["xReact: Ali buys a book",
+#                     "xReact: Ali fell on his knees"]))
+
+
+# +
+df = pd.read_csv("data/eval.tsv", sep="\t").astype(str)
+# Prepare the data for testing
+#df = df.groupby('input_text')['target_text'].apply(list)
+def my_eval(df, prefix):
+    df = df.groupby(['prefix','input_text'], as_index=False).agg({'target_text':lambda x: list(x)})
+    truth_values = df.loc[df["prefix"] == prefix]["target_text"].tolist()
+    input_values = df.loc[df["prefix"] == prefix]["input_text"].tolist()
+    input_values = [prefix + ": " + str(input_text) for input_text in input_values]
+
+    pred_values = model.predict(input_values)
+    preds = [item[0] for item in pred_values]
+    
+    refs = [[] for i in range(5)]
+    for item in truth_values:
+        for i in range(5):
+            if i < len(item):
+                refs[i].append(item[i])
+            else:
+                refs[i].append('')  
+    bleu_score = sacrebleu.corpus_bleu(preds, refs)
+    return bleu_score, input_values, truth_values, pred_values
+    
+tasks = list(set(df["prefix"].tolist()))
+
+
+# +
+bleu_score, input_values, truth_values, pred_values = my_eval(df, task)
+
+print("BLEU for " + task + ":" + str(bleu_score.score))
+
+sel_args = ["length_penalty", "repetition_penalty", "num_beams", "do_sample", "top_p", "top_k"]
+Path(f'{model_name}/results/').mkdir(exist_ok=True, parents=True)
+cc = 1
+args = model_args.get_args_for_saving()
+res_name = "res_" + task
+for key, val in args.items():
+    if key in sel_args:
+        res_name += "_" + key + "_" + str(val)
+
+with open(f'{model_name}/results/{res_name}.txt', 'w') as f:
+        print("BLEU:" + str(bleu_score.score), file=f)
+        for inp, truth, pred in zip(input_values, truth_values, pred_values):
+            inp = inp.replace("as a result PersonX want","")
+            cc += 1
+            if cc < 10:
+                print(inp,":", truth,"--", pred[0])
+            print(inp,":", truth,"--", pred, file =f)
+        print("Arguments:", file=f)
+        for key, val in args.items():
+            print(key, "=", val, file=f)
+# -
 
 
