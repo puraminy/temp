4513de76e1c3c586260bc5f96b122b0f2c85946d
Ahmad <pouramini@gmail.com>
2021-03-19 13:58:19 +0330

    Update and rename atomic_to_csv.py to prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -1,19 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.5.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
-# +
 import pandas as pd
 import json
 
