d97126c81b778e523037a73bcdec240702d936be
Ahmad <pouramini@gmail.com>
2021-03-19 13:33:14 +0330

    ... Adding atomic_to_csv.py

---
+++ atomic_to_csv.py
@@ -0,0 +1,60 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.5.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import pandas as pd
+import json
+
+
+import csv
+from pathlib import Path
+from tqdm import tqdm
+    
+Path("data/").mkdir(parents=True, exist_ok=True)
+
+       
+def write_tsv(src, split, sel_cols):
+    df = pd.read_csv(src,index_col=0)
+    df.iloc[:,:9] = df.iloc[:,:9].apply(lambda col: col.apply(json.loads))    
+    skip =0
+    fname = "data/" + split + ".tsv"    
+    with open(fname, 'w') as tsvfile:
+        writer = csv.writer(tsvfile, delimiter='\t')
+        writer.writerow(["prefix", "input_text", "target_text"])
+        for row, cols in tqdm(df.iterrows(), total=df.shape[0]): 
+            #row = row + " as a result PersonX want "
+            for col in df.columns:
+                for c in cols[col]:
+                    if len(c) >= 60:
+                        skip+=1
+
+                    if ((sel_cols == ["All"] or col in sel_cols)
+                        and col != "prefix" 
+                        and col != "split" and len(c) < 60):
+                        writer.writerow([col, row, c])                
+
+    
+print("train set ...")
+write_tsv("v4_atomic_trn.csv", "train", ["All"])# ["xWant","xAttr", "xNeed"])
+print("test set ...")
+write_tsv("v4_atomic_tst.csv", "test", ["All"])#, ["xWant","xAttr", "xNeed"])
+print("eval set ...")
+write_tsv("v4_atomic_dev.csv", "eval", ["All"])#, ["xWant","xAttr", "xNeed"])
+
+
+# -
+
+
+
